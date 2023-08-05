9a80e8266b571a14fbc7a0d24248964bcbb5514c
avliserdna <andre.christian.silva@gmail.com>
2023-08-04 17:12:13 -0700

    Finished single linked list practice.

--- linked_list_project_practice/data-structures-linked-lists-queue-starter-main/data-structures-linked-lists-queue-starter-main/implementation/01-singly-linked-list.js
+++ linked_list_project_practice/data-structures-linked-lists-queue-starter-main/data-structures-linked-lists-queue-starter-main/implementation/01-singly-linked-list.js
@@ -12,10 +12,24 @@ class SinglyLinkedList {
         this.length = 0;
     }
 
-    addToHead(val) { 
+    noHead(head) {
+        this.head = head;
+        return this.head
+    }
+    addToHead(val) {
         // Add node of val to head of linked list
-
+        const newNode = new SinglyLinkedNode(val, null)
+        if (!this.head) {
+            this.noHead(newNode)
+        }
+        else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++
+        return this
         // Write your hypothesis on the time complexity of this method here
+        // o(1) time complexity, because you're just pointing to current data.
     }
 
     addToTail(val) {
@@ -23,43 +37,82 @@ class SinglyLinkedList {
         // Write your hypothesis on the time complexity of this method here
 
         // Add node of val to tail of linked list
-        let newNode = new SinglyLinkedNode(data);
+        const newNode = new SinglyLinkedNode(val, null);
 
-        if (!head) {
-            head = newNode;
-            return head;
+        if (!this.head) {
+            this.noHead(newNode)
         }
-
-        let curr = head;
-        while (curr) {
-            curr = current.next;
+        else {
+            let current = this.head
+            while (current.next) {
+                current = current.next
+            }
+            current.next = newNode
+            current = newNode
         }
-        curr.next = newNode;
-
-        return head;
+        this.length++
+        return this
+        // o(1) because you're also just pointing to pre-established data
     }
 
     removeFromHead() {
         // Remove node at head
-
+        if (this.head) {
+            const removedHead = this.head
+            this.head = this.head.next
+            this.length--
+            return removedHead
+        }
+        else {
+            return undefined
+        }
         // Write your hypothesis on the time complexity of this method here
-    }
+        // o(1) because you're accessing already established data with just using pointers
+ }
 
     removeFromTail() {
         // Remove node at tail
+        if (this.head && this.length === 1) {
+            const current = this.head
+            this.head = null;
+            this.length--
+            return current
+        }
+        else if (this.head) {
+            let current = this.head;
+            let prev = null;
+            while (current.next) {
+                prev = current
+                current = current.next
+            }
+            prev.next = null;
+            this.length--
+            return current
+        }
+        else {
+            return undefined
+        }
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtHead() {
         // Return value of head node
-
+        return this.head ? this.head.value : undefined
         // Write your hypothesis on the time complexity of this method here
     }
 
     print() {
         // Print out the linked list
-        
+        if (this.head) {
+            let current = this.head;
+            while (current.next) {
+                console.log(current.value)
+                current = current.next
+            }
+            console.log(current.value)
+            return
+        }
         // Write your hypothesis on the time complexity of this method here
     }
 }
